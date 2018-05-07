760e04a89f5ff57553f80a66514a97045cf460d4
Billin9 <Billin9@163.com>
2018-05-07 10:49:24 +0800

    Merge branch 'master' of https://github.com/purcell/emacs.d

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -1,4 +1,6 @@
+;;; -*- lexical-binding: t -*-
 (when (maybe-require-package 'ivy)
+  (add-hook 'after-init-hook 'ivy-mode)
   (after-load 'ivy
     (setq-default ivy-use-virtual-buffers t
                   ivy-virtual-abbreviate 'fullpath
@@ -46,20 +48,20 @@
             ((executable-find "ack") 'counsel-ack))))
       (when search-function
         (defun sanityinc/counsel-search-project (initial-input &optional use-current-dir)
-          "Search using `counsel-rg' or similar from the project root for INITIAL-INPUT.
+          "Search using `counsel-ag' from the project root for INITIAL-INPUT.
 If there is no project root, or if the prefix argument
 USE-CURRENT-DIR is set, then search from the current directory
 instead."
-      (interactive (list (thing-at-point 'symbol)
-                         current-prefix-arg))
-      (let ((current-prefix-arg)
-            (dir (if use-current-dir
-                     default-directory
-                   (condition-case err
-                       (projectile-project-root)
-                     (error default-directory)))))
-        (counsel-ag initial-input dir)))
-    (global-set-key (kbd "M-?") 'sanityinc/counsel-ag-project)))
+          (interactive (list (thing-at-point 'symbol)
+                             current-prefix-arg))
+          (let ((current-prefix-arg)
+                (dir (if use-current-dir
+                         default-directory
+                       (condition-case err
+                           (projectile-project-root)
+                         (error default-directory)))))
+            (funcall search-function initial-input dir)))))
+    (global-set-key (kbd "M-?") 'sanityinc/counsel-search-project)))
 
 
 (when (maybe-require-package 'swiper)
